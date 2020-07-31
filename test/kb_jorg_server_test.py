0af0954d5ce2cc86a6e9af1c03fad974dec00052
Sean Jungbluth <jungbluth.sean@gmail.com>
2020-07-31 08:04:56 -0700

    Test overhaul

--- test/kb_jorg_server_test.py
+++ test/kb_jorg_server_test.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 import os
 import time
+import shutil
 import unittest
 from configparser import ConfigParser
 
 from kb_jorg.kb_jorgImpl import kb_jorg
 from kb_jorg.kb_jorgServer import MethodContext
-from kb_jorg.authclient import KBaseAuth as _KBaseAuth
+from kb_jorg.authclient import KBaseAuth as KBaseAuth
 
+from kb_jorg.Utils.JorgUtil import JorgUtil
+
+
+from installed_clients.AssemblyUtilClient import AssemblyUtil
+from installed_clients.DataFileUtilClient import DataFileUtil
+from installed_clients.ReadsUtilsClient import ReadsUtils
 from installed_clients.WorkspaceClient import Workspace
 
 
@@ -15,7 +22,8 @@ class kb_jorgTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        token = os.environ.get('KB_AUTH_TOKEN', None)
+        # token = os.environ.get('KB_AUTH_TOKEN', None)
+        cls.token = os.environ.get('KB_AUTH_TOKEN', None)
         config_file = os.environ.get('KB_DEPLOYMENT_CONFIG', None)
         cls.cfg = {}
         config = ConfigParser()
@@ -24,12 +32,12 @@ class kb_jorgTest(unittest.TestCase):
             cls.cfg[nameval[0]] = nameval[1]
         # Getting username from Auth profile for token
         authServiceUrl = cls.cfg['auth-service-url']
-        auth_client = _KBaseAuth(authServiceUrl)
-        user_id = auth_client.get_user(token)
+        auth_client = KBaseAuth(authServiceUrl)
+        user_id = auth_client.get_user(cls.token)
         # WARNING: don't call any logging methods on the context object,
         # it'll result in a NoneType error
         cls.ctx = MethodContext(None)
-        cls.ctx.update({'token': token,
+        cls.ctx.update({'token': cls.token,
                         'user_id': user_id,
                         'provenance': [
                             {'service': 'kb_jorg',
@@ -43,8 +51,16 @@ class kb_jorgTest(unittest.TestCase):
         cls.scratch = cls.cfg['scratch']
         cls.callback_url = os.environ['SDK_CALLBACK_URL']
         suffix = int(time.time() * 1000)
-        cls.wsName = "test_ContigFilter_" + str(suffix)
-        ret = cls.wsClient.create_workspace({'workspace': cls.wsName})  # noqa
+        cls.wsName = "test_kb_jorg_" + str(suffix)
+        # ret = cls.wsClient.create_workspace({'workspace': cls.wsName})  # noqa
+
+        cls.ws_info = cls.wsClient.create_workspace({'workspace': cls.wsName})  # noqa
+        cls.dfu = DataFileUtil(os.environ['SDK_CALLBACK_URL'], token=cls.token)
+        cls.ru = ReadsUtils(os.environ['SDK_CALLBACK_URL'], token=cls.token)
+        cls.au = AssemblyUtil(os.environ['SDK_CALLBACK_URL'], token=cls.token)
+        cls.jorg_runner = JorgUtil(cls.cfg)
+        cls.prepare_data()
+
 
     @classmethod
     def tearDownClass(cls):
@@ -53,15 +69,134 @@ class kb_jorgTest(unittest.TestCase):
             print('Test workspace was deleted')
 
     # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
-    def test_your_method(self):
-        # Prepare test objects in workspace if needed using
-        # self.getWsClient().save_objects({'workspace': self.getWsName(),
-        #                                  'objects': []})
+
+    @classmethod
+    def prepare_data(cls):
+        """
+        Lets put everything on workspace
+        """
         #
-        # Run your method by
-        # ret = self.getImpl().your_method(self.getContext(), parameters...)
+        # READS 1
+        # building paired-end library
+        pe1_reads_filename = 'bin.186_paired-end_10K-seqs.fastq.gz'
+        pe1_reads_path = os.path.join(cls.scratch, pe1_reads_filename)
+
+        # gets put on scratch. "work/tmp" is scratch
+        shutil.copy(os.path.join("data", pe1_reads_filename), pe1_reads_path)
+
+        int1_reads_params = {
+            'fwd_file': pe1_reads_path,
+            'sequencing_tech': 'Unknown',
+            'wsname': cls.ws_info[1],
+            'name': 'MyInterleavedLibrary1',
+            'interleaved': 'true'
+        }
+
+        #from scratch upload to workspace
+
+        cls.int1_oldstyle_reads_ref = cls.ru.upload_reads(int1_reads_params)['obj_ref']
+
+        # READS 2
+        # building paired-end library
+        pe2_reads_filename = 'bin.186_paired-end_100K-seqs.fastq.gz'
+        pe2_reads_path = os.path.join(cls.scratch, pe2_reads_filename)
+
+        # gets put on scratch. "work/tmp" is scratch
+        shutil.copy(os.path.join("data", pe2_reads_filename), pe2_reads_path)
+
+        int2_reads_params = {
+            'fwd_file': pe2_reads_path,
+            'sequencing_tech': 'Unknown',
+            'wsname': cls.ws_info[1],
+            'name': 'MyInterleavedLibrary2',
+            'interleaved': 'true'
+        }
+
+        #from scratch upload to workspace
+        cls.int2_oldstyle_reads_ref = cls.ru.upload_reads(int2_reads_params)['obj_ref']
+
+        # READS 3
+        # building paired-end library
+        pe3_reads_filename = 'bin.186_paired-end_500K-seqs.fastq.gz'
+        pe3_reads_path = os.path.join(cls.scratch, pe3_reads_filename)
+
+        # gets put on scratch. "work/tmp" is scratch
+        shutil.copy(os.path.join("data", pe3_reads_filename), pe3_reads_path)
+
+        int3_reads_params = {
+            'fwd_file': pe3_reads_path,
+            'sequencing_tech': 'Unknown',
+            'wsname': cls.ws_info[1],
+            'name': 'MyInterleavedLibrary3',
+            'interleaved': 'true'
+        }
+
+        #from scratch upload to workspace
+        cls.int3_oldstyle_reads_ref = cls.ru.upload_reads(int3_reads_params)['obj_ref']
+
+        # READS 4
+        # building Interleaved library
+        pe4_reads_filename = 'bin.186.fastq.gz'
+        pe4_reads_path = os.path.join(cls.scratch, pe4_reads_filename)
+
+        # gets put on scratch. "work/tmp" is scratch
+        shutil.copy(os.path.join("data", pe4_reads_filename), pe4_reads_path)
+
+        int4_reads_params = {
+            'fwd_file': pe4_reads_path,
+            'sequencing_tech': 'Unknown',
+            'wsname': cls.ws_info[1],
+            'name': 'MyInterleavedLibrary4',
+            'interleaved': 'true'
+        }
+
+        #from scratch upload to workspace
+        cls.int4_oldstyle_reads_ref = cls.ru.upload_reads(int4_reads_params)['obj_ref']
+
         #
-        # Check returned data with
-        # self.assertEqual(ret[...], ...) or other unittest methods
-        ret = self.serviceImpl.run_kb_jorg(self.ctx, {'workspace_name': self.wsName,
-                                                             'parameter_1': 'Hello World!'})
+        # building Assembly
+        #
+        assembly_filename1 = 'bin.186.fa'
+        cls.assembly_filename_path1 = os.path.join(cls.scratch, assembly_filename1)
+        shutil.copy(os.path.join("data", assembly_filename1), cls.assembly_filename_path1)
+
+        # from scratch upload to workspace
+        assembly_params1 = {
+            'file': {'path': cls.assembly_filename_path1},
+            'workspace_name': cls.ws_info[1],
+            'assembly_name': 'MyAssembly1'
+        }
+
+        # puts assembly object onto shock
+        cls.assembly_ref1 = cls.au.save_assembly_from_fasta(assembly_params1)
+
+    def getWsClient(self):
+        return self.__class__.wsClient
+
+    def getWsName(self):
+        return self.ws_info[1]
+
+    def getImpl(self):
+        return self.__class__.serviceImpl
+
+    def getContext(self):
+        return self.__class__.ctx
+
+
+    def test_run_jorg_default(self):
+        method_name = 'test_run_jorg_default'
+        print ("\n=================================================================")
+        print ("RUNNING "+method_name+"()")
+        print ("=================================================================\n")
+
+        # jorg should run to completion here
+        ret = self.getImpl().run_kb_jorg(self.getContext(),
+                                            {'workspace_name': self.getWsName(),
+                                             'assembly_ref': self.assembly_ref1,
+                                             'read_mapping_tool': 'bbmap',
+                                             'kmer_size': 33,
+                                             'min_coverage': 30,
+                                             'num_iterations': 5,
+                                             'high_contig_num': 'no',
+                                             'single_end_reads': 'no',
+                                             'reads_file': [self.int1_oldstyle_reads_ref] })
