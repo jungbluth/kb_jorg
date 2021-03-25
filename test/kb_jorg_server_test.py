4bf1531cf3205c22f5b8624e9eea3a57647b254c
Sean Jungbluth <jungbluth.sean@gmail.com>
2021-03-25 09:28:25 -0700

    Testing

--- test/kb_jorg_server_test.py
+++ test/kb_jorg_server_test.py
@@ -78,7 +78,8 @@ class kb_jorgTest(unittest.TestCase):
         #
         # READS 1
         # building paired-end library
-        pe1_reads_filename = 'bin.186.fastq'
+        #pe1_reads_filename = 'bin.186.fastq'
+        pe1_reads_filename = 'bin.186_paired-end_100K-seqs.fastq'
         pe1_reads_path = os.path.join(cls.scratch, pe1_reads_filename)
 
         # gets put on scratch. "work/tmp" is scratch
